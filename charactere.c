480c8217c4e86f9d8d4c6cbea2f5416e37f0f184
KhalifeMehdi <132369396+KhalifeMehdi@users.noreply.github.com>
2023-09-15 16:37:06 +0200

    Update charactere.c

--- charactere.c
+++ charactere.c
@@ -2,6 +2,33 @@
 #include <stdio.h>
 
 #include "charactere.h"
+//mettre des define ?
+
+
+void chargerMario(Personnage* mario, Map* map, SDL_Renderer *renderer)
+{
+	Personnage *mario = malloc(sizeof(Personnage));
+    mario -> jump=0;
+    mario -> jumptime=0;
+    mario -> gravite=0;
+    mario -> direction=0;
+    mario -> temp=;
+    mario -> win=0;
+    mario -> invisible=0;
+    mario -> lvl;
+}
+
+void afficherPerso(Personnage* mario, int xscroll, int yscroll , SDL_Renderer *renderer) {
+
+   
+}
+
+
+//retourne soit numéro1 soit numéro2 pour alterner entre 2 sprite sur un mouvement.
+int chooseSpriteMovement(Personnage* mario, int numero1, int numero2) {
+    
+    return numero1;
+}
 
 void freePersonnage(Personnage* mario, Personnage **goomba, int nbGoomba) {
     
