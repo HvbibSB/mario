e4e20b81552ee1348aab803e83b79d47a53bbd40
KhalifeMehdi <132369396+KhalifeMehdi@users.noreply.github.com>
2023-09-22 10:53:10 +0200

    Update event.h

--- event.h
+++ event.h
@@ -9,5 +9,11 @@ et si le niveau est fini (win et loose).
     #include "file.h"
     #include "charactere.h"
 
-  
+    void deplacement(Map* map, Personnage* mario, Sprites* imageDecors, Personnage** goombas, int nbGoomba);
+    int collisionHautBas(Map* map, SDL_Rect perso, Sprites* imageDecors);
+    int collisionDecor(Map* map, SDL_Rect perso, Sprites* imageDecors, Personnage* mario);
+    int sortiDecors(Map* map, SDL_Rect perso);
+    void mapScroll(Map* map, Personnage* mario);
+    int niveauFini(SDL_Renderer *renderer, Personnage *mario);
+
 #endif
