385defb7d910c66de07eb738d178748924817940
KhalifeMehdi <132369396+KhalifeMehdi@users.noreply.github.com>
2023-11-10 17:01:09 +0100

    Update game.c

--- game.c
+++ game.c
@@ -6,17 +6,13 @@
 int jouer(SDL_Renderer* renderer) {
     
 	//charger image et personnage.
-    SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window *window;
-    SDL_Texture *Mario1 = loadImage("img/Mario1.png", renderer);
-    SDL_Texture *Mario2 = loadImage("img/Mario2.png", renderer);
-    SDL_Texture *Mario3 = loadImage("img/Mario3.png", renderer);
-    SDL_Texture *Mario4 = loadImage("img/Mario4.png", renderer);
-    SDL_Texture *Mario5 = loadImage("img/Mario5.png", renderer);
-    SDL_Texture *Mario6 = loadImage("img/Mario6.png", renderer);
+    Personnage *mario = malloc(sizeof(Personnage));
+    Map* map = malloc(sizeof(Map));
+    chargerMario(mario,map,renderer);
+    //Là où on chargeait toutes les images de Mario (les 6), nécessite d'être là pour que la ligne 15 fonctionne.
     SDL_Rect rectangleSource = {0,0,500,500};
 	SDL_Rect rectangleDest = {0,0,500,500};
-    SDL_RenderCopy(renderer, Mario1,&rectangleSource,&rectangleDest);
+    //SDL_RenderCopy(renderer, Mario1,&rectangleSource,&rectangleDest);
     SDL_RenderPresent(renderer);
     
 
@@ -39,21 +35,22 @@ int jouer(SDL_Renderer* renderer) {
                     case SDL_KEYDOWN:
             		    switch(event.key.keysym.sym)
 					    {
-
-			    case SDLK_1://appuie sur 1
-                       	    case SDLK_ESCAPE://appuie sur echap    
-				    
-                            continuer = 0;
-                            break;
                             case SDLK_d:
                             SDL_Log("droite");
+                            mario-> direction=1; 
+                            SDL_Log("direction= %d",mario->direction);
                             break;
+
                             case SDLK_q:
                             SDL_Log("gauche");
+                            mario-> direction=2;
+                            SDL_Log("direction= %d", mario->direction);
                             break;
+
                             case SDLK_SPACE:
                             SDL_Log("saut");
                             break;
+
                             case SDLK_ESCAPE:
                             continuer=0;
                             break;
@@ -62,13 +59,14 @@ int jouer(SDL_Renderer* renderer) {
             }
         }
         //ici on choisit if droite =1 alors on déplace vers la droite de 10pixels sur l'axe x (ou y je sais pas encore)
+        //afficher mario et déplacer mario
         SDL_RenderPresent(renderer);
         
     }
     
 	//a vous de compléter, au fur et à mesure, les deux fonctions en dessous pour bien faire le nettoyage. 
-    /*LibererMap(map, sprites);
-    freePersonnage(mario, goomba, nbGoomba);
-*/
+    //LibererMap(map, sprites);
+    freePersonnage(mario); //faudra ajouter ", goomba, nbGoomba" juste après le mario 
+
     return continuer;
 }
